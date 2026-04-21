180a7c734e212f7dd69debb1581f714452ebf234
dayski-47 <businessdayski@gmail.com>
2026-04-20 20:31:51 -0500

    added matrix builder files

---
+++ src/matrix_builder.cpp
@@ -0,0 +1,22 @@
+#include "matrix_builder.h"
+
+
+
+std::vector<std::vector<double>> MatrixBuilder::buildMatrix(const std::vector<Window>& windows){
+    std::vector<std::vector<double>> A;
+    A.reserve(windows.size());
+
+    for (const auto& w : windows){
+        std::vector<double> row = {
+            static_cast<double>(w.packetCount),
+            static_cast<double>(w.totalBytes),
+            static_cast<double>(w.tcpCount),
+            static_cast<double>(w.tcpCount),
+            static_cast<double>(w.udpCount)
+            };
+
+        A.push_back(row);
+    }
+    return A;
+
+}
